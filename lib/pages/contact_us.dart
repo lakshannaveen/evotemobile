8419f9e6f367aa2637c8af3b86b29e9c9052bdf3
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-03-18 10:12:11 +0530

    add email launcher option
    now user can clickble and go to email or phone

--- lib/pages/contact_us.dart
+++ lib/pages/contact_us.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsPage extends StatelessWidget {
   const ContactUsPage({super.key});
 
+  void _launchPhone(String phoneNumber) async {
+    final Uri phoneUri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunchUrl(phoneUri)) {
+      await launchUrl(phoneUri);
+    } else {
+      throw 'Could not launch $phoneUri';
+    }
+  }
+
+  void _launchEmail(String email) async {
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: email,
+      query: 'subject=Contact Us',
+    );
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $emailUri';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    const String phoneNumber =
+        '+94 123 456 789'; // Replace with any contact number
+    const String email =
+        'lakshannaveen578@gmail.com'; // Replace with any contact email
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contact Us'),
@@ -22,22 +50,39 @@ class ContactUsPage extends StatelessWidget {
                   border: Border.all(color: Colors.blue, width: 2),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: const Column(
+                child: Column(
                   children: [
-                    Text(
-                      'If you have any questions, please call the following number:',
+                    const Text(
+                      'If you have any questions, please call the following number or send us an email:',
                       style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10),
-                    Text(
-                      '+94 123 456 789', // Replace with any contact number
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () => _launchPhone(phoneNumber),
+                      child: const Text(
+                        phoneNumber,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () => _launchEmail(email),
+                      child: const Text(
+                        email,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
                       ),
                     ),
                   ],
